e7670ca472acc251ad5f8f8770f5c7f00d5b4550
Mark <mark_zjunlict@zju.edu.cn>
2022-10-25 11:34:49 +0800

    merge two projects into one cmakelists

--- cmake/modules/Findtoluapp.cmake
+++ cmake/modules/Findtoluapp.cmake
@@ -10,13 +10,12 @@
 
 FIND_PATH(TOLUAPP_INCLUDE_DIR tolua++.h
   PATHS
-    "$ENV{TOLUAPP}/include"
-    /usr/local/include
-    /usr/include
+  "$ENV{TOLUAPP}/include"
+  /usr/local/include
+  /usr/include
 	C:/Workspace/Build/tolua++/include
 )
-
-FIND_LIBRARY(TOLUAPP_LIBRARIES toluapp tolua++
+FIND_LIBRARY(TOLUAPP_LIBRARIES toluapp tolua++ tolua++5.1
   PATHS
   "$ENV{TOLUAPP}/lib"
   /usr/local/lib
@@ -25,16 +24,27 @@ FIND_LIBRARY(TOLUAPP_LIBRARIES toluapp tolua++
 	C:/Workspace/Build/tolua++/lib/Debug
 	C:/Workspace/Build/tolua++/lib/Release
 )
-
+FIND_PROGRAM(TOLUAPP_EXECUTABLE tolua++ toluapp tolua++5.1
+  PATHS
+  "$ENV{TOLUAPP}/bin"
+  /usr/local/bin
+  /usr/bin
+  C:/Workspace/Build/tolua++/bin
+  C:/Workspace/Build/tolua++/bin/Debug
+  C:/Workspace/Build/tolua++/bin/Release
+)
 SET(TOLUAPP_FOUND 0)
 IF(TOLUAPP_INCLUDE_DIR)
   IF(TOLUAPP_LIBRARIES)
-    SET(TOLUAPP_FOUND 1)
-    MESSAGE(STATUS "Found TOLUAPP")
+    IF(TOLUAPP_EXECUTABLE)
+      SET(TOLUAPP_FOUND 1)
+      MESSAGE(STATUS "Found TOLUAPP")
+    ENDIF(TOLUAPP_EXECUTABLE)
   ENDIF(TOLUAPP_LIBRARIES)
 ENDIF(TOLUAPP_INCLUDE_DIR)
 
 MARK_AS_ADVANCED(
   TOLUAPP_INCLUDE_DIR
   TOLUAPP_LIBRARIES
+  TOLUAPP_EXECUTABLE
 ) 
