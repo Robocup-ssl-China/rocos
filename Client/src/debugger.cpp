ad8ee00a82a467f8a4ae84b69bc73bdd2bff4611
Herry Li <ausksyc@outlook.com>
2026-04-12 22:04:46 +0800

    [Client] 修复Debug信息概率性无法接收的bug，统一使用 Qt 原始的readyRead

--- Client/src/debugger.cpp
+++ Client/src/debugger.cpp
@@ -1,45 +1,33 @@
 #include "debugger.h"
 #include "globaldata.h"
-#include <thread>
-namespace {
-std::thread* blueDebuggerThread = nullptr;
-std::thread* yellowDebuggerThread = nullptr;
-}
+
 Debugger::Debugger(QObject *parent) : QObject(parent){
-//    QObject::connect(&receiverBlue,SIGNAL(readyRead()),this,SLOT(receiveBlue()),Qt::DirectConnection);
-    if(receiverBlue.bind(QHostAddress::AnyIPv4,ZSS::Athena::DEBUG_MSG_RECEIVE[0], QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint)){
-        blueDebuggerThread = new std::thread([=,this] {receiveBlue();});
-        blueDebuggerThread->detach();
+    if (receiverBlue.bind(QHostAddress::AnyIPv4,
+                          ZSS::Athena::DEBUG_MSG_RECEIVE[0],
+                          QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint)) {
+        QObject::connect(&receiverBlue, &QUdpSocket::readyRead, this, &Debugger::receiveBlue);
     }
-//    QObject::connect(&receiverYellow,SIGNAL(readyRead()),this,SLOT(receiveYellow()),Qt::DirectConnection);
-    if(receiverYellow.bind(QHostAddress::AnyIPv4,ZSS::Athena::DEBUG_MSG_RECEIVE[1], QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint)){
-        yellowDebuggerThread = new std::thread([=,this] {receiveYellow();});
-        yellowDebuggerThread->detach();
+    if (receiverYellow.bind(QHostAddress::AnyIPv4,
+                            ZSS::Athena::DEBUG_MSG_RECEIVE[1],
+                            QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint)) {
+        QObject::connect(&receiverYellow, &QUdpSocket::readyRead, this, &Debugger::receiveYellow);
     }
 }
 
 void Debugger::receiveBlue(){
     auto& datagram = GlobalData::instance()->debugBlueMessages;
-    while(true){
-        std::this_thread::sleep_for(std::chrono::microseconds(500));
-        while (receiverBlue.state() == QUdpSocket::BoundState && receiverBlue.hasPendingDatagrams()) {
-            GlobalData::instance()->debugMutex.lock();
-            datagram.resize(receiverBlue.pendingDatagramSize());
-            receiverBlue.readDatagram(datagram.data(),datagram.size());
-            GlobalData::instance()->debugMutex.unlock();
-        }
+    while (receiverBlue.state() == QUdpSocket::BoundState && receiverBlue.hasPendingDatagrams()) {
+        QMutexLocker lock(&GlobalData::instance()->debugMutex);
+        datagram.resize(receiverBlue.pendingDatagramSize());
+        receiverBlue.readDatagram(datagram.data(), datagram.size());
     }
 }
 
 void Debugger::receiveYellow(){
     auto& datagram = GlobalData::instance()->debugYellowMessages;
-    while(true){
-        std::this_thread::sleep_for(std::chrono::microseconds(500));
-        while (receiverYellow.state() == QUdpSocket::BoundState && receiverYellow.hasPendingDatagrams()) {
-            GlobalData::instance()->debugMutex.lock();
-            datagram.resize(receiverYellow.pendingDatagramSize());
-            receiverYellow.readDatagram(datagram.data(),datagram.size());
-            GlobalData::instance()->debugMutex.unlock();
-        }
+    while (receiverYellow.state() == QUdpSocket::BoundState && receiverYellow.hasPendingDatagrams()) {
+        QMutexLocker lock(&GlobalData::instance()->debugMutex);
+        datagram.resize(receiverYellow.pendingDatagramSize());
+        receiverYellow.readDatagram(datagram.data(), datagram.size());
     }
 }
